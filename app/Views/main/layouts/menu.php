bc99706851bc39f17b632b59d7cca8a15e1d58b0
Dam Trung Kien <damtrungkien229@gmail.com>
2026-01-02 14:23:47 +0700

    kien

--- app/Views/main/layouts/menu.php
+++ app/Views/main/layouts/menu.php
@@ -10,30 +10,30 @@ if ($viTri === 1) {
 ?>
 <div class="grid-menu">
     <div class="menu-item" onclick="window.location.href='<?= $resourceLink ?>'">
-        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/Group.png" alt=""></div>
+        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/tabler_home-check.svg" alt=""></div>
         <span>Kho tài<br>nguyên</span>
     </div>
 
     <div class="menu-item" onclick="window.location.href='<?= BASE_URL ?>/collection'">
-        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/Group (1).png" alt=""></div>
+        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/icon-park-outline_bookmark.svg" alt=""></div>
         <span>Bộ sưu tập</span>
     </div>
 
     <div class="menu-item" onclick="window.location.href='<?= BASE_URL ?>/auto-match'">
-        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/Vector (2).png" alt=""></div>
+        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/ant-design_user-switch-outlined.svg" alt=""></div>
         <span>QL khách-<br>Tự khớp</span>
     </div>
 
     <div class="menu-item" onclick="window.location.href='<?= BASE_URL ?>/report_list'">
-        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/Vector (3).png" alt=""></div>
+        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/ic_round-bar-chart.svg" alt=""></div>
         <span>Báo cáo<br>dẫn khách</span>
     </div>
     <div class="menu-item" onclick="window.location.href='<?= BASE_URL ?>/notification'">
-        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/Vector (4).png" alt=""></div>
+        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/mdi_cart-outline.svg" alt=""></div>
         <span>Thông báo<br>vụ chốt</span>
     </div>
-    <div class="menu-item" onclick="window.location.href='<?= BASE_URL ?>/policy'">
-        <div class="icon-box"><i class="fa-solid fa-scale-balanced"></i></div>
+    <div class=" menu-item" onclick="window.location.href='<?= BASE_URL ?>/policy'">
+        <div class="icon-box"><img src="<?= BASE_URL ?>/icon/mdi_scale-balance.svg" alt=""></div>
         <span>Quy định và<br>hướng dẫn</span>
     </div>
 </div>
